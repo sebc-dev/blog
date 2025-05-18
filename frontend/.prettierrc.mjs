172c3d45646c23282f754ee3cbc65ddcab227717
NegusSalomon <chauveau.sebastien@gmail.com>
2025-05-18 17:24:10 +0200

    ✨ Mise à jour de la configuration Prettier pour inclure des options de style, ajout du nom du projet dans package.json, activation des globals dans la configuration Vitest, amélioration de l'accessibilité du composant ThemeSwitcher avec un attribut aria, et ajout de métadonnées par défaut dans BaseLayout. Mise à jour de la page d'accueil pour refléter ces changements.

--- frontend/.prettierrc.mjs
+++ frontend/.prettierrc.mjs
@@ -2,6 +2,11 @@
 /** @type {import("prettier").Config} */
 export default {
 	plugins: ['prettier-plugin-astro'],
+	tabWidth: 2,
+	semi: true,
+	singleQuote: true,
+	trailingComma: 'es5',
+	printWidth: 100,
 	overrides: [
 		{
 			files: '*.astro',
