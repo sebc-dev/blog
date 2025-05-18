2d6d81a3efd836a4648836291c543af6f86f80b8
NegusSalomon <chauveau.sebastien@gmail.com>
2025-05-18 11:55:11 +0200

    ✨ Mise à jour de la configuration d'Astro pour intégrer ESLint avec des règles spécifiques à Astro, ajout de l'exclusion des fichiers node_modules dans tsconfig, et amélioration du composant Footer pour utiliser une variable pour l'année actuelle. Modification du Header pour inclure un slot, et simplification de la gestion des propriétés dans BaseLayout.

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,4 +3,24 @@ import {defineConfig} from 'eslint/config';
 
 export default defineConfig([
 	config,
+	{
+		env: {
+			browser: true,
+			es2024: true,
+		},
+		plugins: ['astro'],
+		extends: ['plugin:astro/recommended'],
+		overrides: [
+			{
+				files: ['*.astro'],
+				parser: 'astro-eslint-parser',
+				parserOptions: {
+					parser: '@typescript-eslint/parser',
+				},
+				rules: {
+					// Add or override any Astro-specific rules here
+				},
+			},
+		],
+	}
 ]);
