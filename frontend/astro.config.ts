a3f0fba6855d8016e699a0811ac171e4cddfed5c
NegusSalomon <chauveau.sebastien@gmail.com>
2025-05-22 06:40:00 +0200

    ✨ Mise à jour du fichier `.gitignore` pour inclure `dev-certs/`. Révision du fichier `docker-compose.yml` pour simplifier la configuration des services, en ajustant les ports et en modifiant les règles de routage pour le frontend et le backend. Amélioration de la configuration du serveur dans `astro.config.ts` pour activer le polling. Changement des identifiants de connexion dans `application.yml` pour le service de base de données. Révision du `Dockerfile` pour le frontend avec une configuration optimisée pour PNPM.

--- frontend/astro.config.ts
+++ frontend/astro.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    vite: {
-        plugins: [
-          tailwindcss()
-        ],
+  server: {
+    host: '0.0.0.0',
+    port: 4321,
+  },
+  vite: {
+    plugins: [tailwindcss()],
+    server: {
+      watch: {
+        usePolling: true, // Important pour Docker sur certains systèmes
+        interval: 100, // Intervalle de polling réduit
       },
+    },
+  },
 });
